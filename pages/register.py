30408e3bd1e539a34370a7798345b024613c491e
envi <alexgolubev1404@gmail.com>
2024-09-24 05:44:06 -0700

    Update register.py

--- pages/register.py
+++ pages/register.py
@@ -8,31 +8,4 @@ def tpl_register(page: Page):
     page.vertical_alignment = MainAxisAlignment.CENTER
     page.theme_mode = ThemeMode.SYSTEM
 
-    text_username: TextField = TextField(label='Username', text_align=TextAlign.LEFT, width=200, on_change=validate)
-    text_password: TextField = TextField(label='Password', text_align=TextAlign.LEFT, width=200, password=True, on_change=validate)
-    checkbox_signup: Checkbox = Checkbox(label='I agree to the Terms and Conditions', value=False)
-    button_submit: ElevatedButton = ElevatedButton(text='Sign Up', disabled=True)
-
-    def validate(e: ControlEvent):
-        if all([text_password.value, text_username.value, checkbox_signup.value]):
-            button_submit.disabled = False
-        else:
-            button_submit.disabled = True
-
-        page.update()
-
-    page.add(
-        Row(
-            controls=[
-                Column(
-                    [
-                        text_username,
-                        text_password,
-                        checkbox_signup,
-                        button_submit,
-                    ]
-                )
-            ],
-            alignment=MainAxisAlignment.CENTER
-        )
-    )
+    page.add(Text('register page'))
