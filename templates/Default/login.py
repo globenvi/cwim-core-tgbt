f367cf473805432bc0c92aa5bf773efe375d43c6
envi <alexgolubev1404@gmail.com>
2024-09-30 11:43:50 +0700

    Default Template Update

--- templates/Default/login.py
+++ templates/Default/login.py
@@ -1,20 +1,72 @@
-from flet import Column, TextField, ElevatedButton, Text, Container
+from attr.setters import validate
+from flet import *
+from flet_core.alignment import center
+from flet_core.cupertino_icons import BOLD_UNDERLINE
 
-def tpl_login(page):
-    # Контейнер с формой входа
-    form = Container(
-        content=Column(
-            controls=[
-                Text("Авторизация", size=20, weight="bold"),
-                TextField(label="Имя пользователя"),
-                TextField(label="Пароль", password=True),
-                ElevatedButton("Войти", on_click=lambda e: page.go("/index")),
-            ],
-            alignment="center"
-        ),
-        alignment="center",
-        width=300,
-        padding=10,
+
+def main(page: Page):
+    page.title = 'Авторизация'
+    page.theme_mode = ThemeMode.SYSTEM
+    page.vertical_alignment = MainAxisAlignment.CENTER
+    page.horizontal_alignment = CrossAxisAlignment.CENTER
+
+
+    user_login_input = TextField(label='Login')
+    user_password_input = TextField(label='Password', password=True)
+    check_remember = Checkbox(label='Remember me')
+
+    def err_snack(e):
+        page.snack_bar = SnackBar(content=Text('Заполните все поля!', weight=BOLD_UNDERLINE))
+        page.snack_bar.bgcolor = colors.RED
+        page.snack_bar.open = True
+        page.update()
+
+    def validate_form(e):
+        if not user_login_input.value or not user_password_input.value:
+            err_snack(e)
+
+
+    submit_button = CupertinoFilledButton('Войти', on_click=validate_form, alignment=center)
+
+    page.add(
+        Container(
+            # border= border.all(color=colors.GREY_100),
+            border_radius=5,
+            margin=10,
+            bgcolor=colors.ON_SECONDARY,
+            width=350,
+            content=Column(
+                [
+                    Row(
+                        [
+                            Column(
+                                [
+                                    Container(
+                                        bgcolor=colors.PRIMARY,
+                                        width=350,
+                                        padding=10,
+                                        content=Text(page.title, color=colors.ON_PRIMARY, size=25),
+                                    ),
+                                    Container(
+                                        padding=25,
+                                        content=Column(
+                                            [
+                                                user_login_input,
+                                                user_password_input,
+                                                check_remember,
+                                                submit_button
+                                            ],
+                                        )
+                                    )
+                                ],
+                            )
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    )
+                ]
+            )
+        )
     )
-    page.controls.append(form)
     page.update()
+
+app(main)
