adbe62e68661e43d6eaa6e2a21b7d43a8f334e6f
envi <alexgolubev1404@gmail.com>
2024-10-01 14:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def main(page: Page):
 # Запуск Flet
 if __name__ == "__main__":
     import flet as ft
-    ft.app(target=main, view=ft.AppView.FLET_APP, port=57060)
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=57060)
