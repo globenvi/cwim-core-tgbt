60a3c309eace6ccfd7020cbff72d525cf6d6c110
envi <alexgolubev1404@gmail.com>
2024-10-01 14:16:07 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -1,5 +1,20 @@
-from flet import Column, Text, Container
+from flet import *
 
 def tpl_index(page):
     page.title = 'Главная страница'
-    page.add(Text('Index'))
+    page.theme_mode = ThemeMode.SYSTEM
+    page.vertical_alignment = MainAxisAlignment.CENTER
+    page.horizontal_alignment = CrossAxisAlignment.CENTER
+
+    page.add(
+        Container(
+            # border=border.all(color=colors.GREY_100),
+            border_radius=5,
+            margin=10,
+            padding=10,
+            bgcolor=colors.ON_SECONDARY,
+            width=350,
+            content=Text('Главная страница', color=colors.ON_PRIMARY, size=25)
+        )
+    )
+
