d6f2211859ebb83e4fdd5b21ee68c911764286d5
envi <alexgolubev1404@gmail.com>
2024-10-01 17:20:27 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -3,13 +3,19 @@ from flet import *
 def tpl_index(page):
     page.title = 'Главная страница'
     page.theme_mode = ThemeMode.SYSTEM
-    # The value is automatically converted back to the original type
-    login = page.client_storage.get("login")
+    page.vertical_alignment = VerticalAlignment.CENTER
 
-    role = page.client_storage.get("role")
-
-    print(f'Login: {login}\n, Role: {role}')
-    # colors = ["red", "green", "blue"]
     page.add(
-        Text('WELCOME TO HOME PAGE')
+        Row(
+            [
+                Container(
+                    border_radius=5,
+                    bgcolor=colors.RED,
+                    width="100%",
+                    height="100%",
+                    padding=20,
+                    margin=20
+                )
+            ]
+        )
     )
