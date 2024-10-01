8c375e9a45782ebff4d24c2b7ed8161c517c42f7
envi <alexgolubev1404@gmail.com>
2024-10-01 13:34:59 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -1,17 +1,5 @@
 from flet import Column, Text, Container
 
 def tpl_index(page):
-    # Контент главной страницы
-    content = Container(
-        content=Column(
-            controls=[
-                Text("Добро пожаловать на главную страницу", size=20, weight="bold"),
-            ],
-            alignment="center"
-        ),
-        alignment="center",
-        width=300,
-        padding=10,
-    )
-    page.controls.append(content)
-    page.update()
+    page.title = 'Главная страница'
+    page.add(Text('Index'))
