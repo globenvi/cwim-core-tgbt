1705761cb55b7d2a55da38ad1ea89fcacc1c95ab
envi <alexgolubev1404@gmail.com>
2024-10-01 14:40:46 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -8,29 +8,6 @@ def tpl_index(page):
         e.control.checked = not e.control.checked
         page.update()
 
-    page.appbar = AppBar(
-        leading=TextButton(icons.MENU),
-        leading_width=40,
-        title=Text('SourceSMS', color=colors.ON_PRIMARY),
-        center_title=False,
-        bgcolor=colors.PRIMARY,
-        actions=[
-            IconButton(icons.WB_SUNNY_OUTLINED),
-            IconButton(icons.FILTER_3),
-            PopupMenuButton(
-                items=[
-                    PopupMenuItem(text="Item 1"),
-                    PopupMenuItem(),  # divider
-                    PopupMenuItem(
-                        text="Checked item", checked=False, on_click=check_item_clicked
-                    ),
-                ]
-            ),
-        ],
-    )
-
-    page.horizontal_alignment = CrossAxisAlignment.CENTER
-
     def handle_dismissal(e):
         page.add(Text("Drawer dismissed"))
 
@@ -61,6 +38,25 @@ def tpl_index(page):
             ),
         ],
     )
+    page.appbar = AppBar(
+        leading=IconButton(icons.MENU, on_click=lambda e: page.open(drawer)),
+        leading_width=40,
+        title=Text('SourceSMS', color=colors.ON_PRIMARY),
+        center_title=False,
+        bgcolor=colors.PRIMARY,
+        actions=[
+            IconButton(icons.WB_SUNNY_OUTLINED),
+            IconButton(icons.FILTER_3),
+            PopupMenuButton(
+                items=[
+                    PopupMenuItem(text="Item 1"),
+                    PopupMenuItem(),  # divider
+                    PopupMenuItem(
+                        text="Checked item", checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            ),
+        ],
+    )
 
-    page.add(ElevatedButton("Show drawer", on_click=lambda e: page.open(drawer)))
     page.add(Text("Body!"))
