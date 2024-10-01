5b75c23f5735929230ae569622083df0274559fd
envi <alexgolubev1404@gmail.com>
2024-10-01 14:30:56 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -4,11 +4,28 @@ def tpl_index(page):
     page.title = 'Главная страница'
     page.theme_mode = ThemeMode.SYSTEM
 
-    page.appbar = AppBar(
-        leading=icons.GITE,
-        leading_width=40,
-        title=Text(page.title),
-        bgcolor=colors.ON_PRIMARY,
-        actions=[]
-    )
+    def check_item_clicked(e):
+        e.control.checked = not e.control.checked
+        page.update()
 
+    page.appbar = AppBar(
+        leading=Icon(icons.PALETTE),
+        leading_width=40,
+        title=Text("AppBar Example"),
+        center_title=False,
+        bgcolor=colors.SURFACE_VARIANT,
+        actions=[
+            IconButton(icons.WB_SUNNY_OUTLINED),
+            IconButton(icons.FILTER_3),
+            PopupMenuButton(
+                items=[
+                    PopupMenuItem(text="Item 1"),
+                    PopupMenuItem(),  # divider
+                    PopupMenuItem(
+                        text="Checked item", checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            ),
+        ],
+    )
+    page.add(Text("Body!"))
