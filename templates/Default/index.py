8d290e4b967ad1753ab9fcf15e581fbebb81cc25
envi <alexgolubev1404@gmail.com>
2024-10-01 14:38:42 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -28,4 +28,39 @@ def tpl_index(page):
             ),
         ],
     )
+
+    page.horizontal_alignment = CrossAxisAlignment.CENTER
+
+    def handle_dismissal(e):
+        page.add(Text("Drawer dismissed"))
+
+    def handle_change(e):
+        page.add(Text(f"Selected Index changed: {e.selected_index}"))
+        # page.close(drawer)
+
+    drawer = NavigationDrawer(
+        on_dismiss=handle_dismissal,
+        on_change=handle_change,
+        controls=[
+            Container(height=12),
+            NavigationDrawerDestination(
+                label="Item 1",
+                icon=icons.DOOR_BACK_DOOR_OUTLINED,
+                selected_icon_content=Icon(icons.DOOR_BACK_DOOR),
+            ),
+            Divider(thickness=2),
+            NavigationDrawerDestination(
+                icon_content=Icon(icons.MAIL_OUTLINED),
+                label="Item 2",
+                selected_icon=icons.MAIL,
+            ),
+            NavigationDrawerDestination(
+                icon_content=Icon(icons.PHONE_OUTLINED),
+                label="Item 3",
+                selected_icon=icons.PHONE,
+            ),
+        ],
+    )
+
+    page.add(ElevatedButton("Show drawer", on_click=lambda e: page.open(drawer)))
     page.add(Text("Body!"))
