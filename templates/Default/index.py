a10eabad720516b7cb66e7d560d41b212c1872a0
envi <alexgolubev1404@gmail.com>
2024-10-01 18:39:57 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -24,7 +24,7 @@ def tpl_index(page: Page, user_group: str):
                 icon=icons.SETTINGS_OUTLINED, selected_icon=icons.SETTINGS, label="Settings"
             ),
             NavigationRailDestination(
-                icon=icons.LOGOUT if user_group != "guest" else icons.LOGIN, selected_icon=icons.LOGOUT if user_group != "guest" else icons.LOGIN, label="Logout" if user_group != "guest" else "Войти"
+                icon=icons.LOGOUT, selected_icon=icons.LOGOUT, label="Logout" if user_group != "guest" else "Войти"
             ),
         ],
         on_change=lambda e: menu_clicked(e),
