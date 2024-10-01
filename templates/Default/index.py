e857038fd7f38c980c4967a66642d41fd5222bf5
envi <alexgolubev1404@gmail.com>
2024-10-01 21:31:07 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -1,22 +1,10 @@
 from flet import *
 
+
 def tpl_index(page: Page):
     page.vertical_alignment = "start"
+
     user_group = page.session.get('user_group')
-
-    # Функция для смены темы
-    def toggle_theme(e):
-        if page.theme_mode == "light":
-            page.theme_mode = "dark"
-        else:
-            page.theme_mode = "light"
-        page.update()
-
-    # Функция для смены цветовой палитры
-    def change_color_palette(e):
-        new_color = colors.AMBER if page.bgcolor != colors.AMBER else colors.PRIMARY
-        page.bgcolor = new_color
-        page.update()
 
     header = AppBar(
         title=Text("SRC CMS"),
@@ -27,34 +15,72 @@ def tpl_index(page: Page):
                 icon=icons.PERSON,
                 tooltip="Профиль",
                 on_click=lambda e: page.go("/profile"),
-                visible=False if user_group == 'guest' else True
+                visible=False if user_group == 'guest' else True # Укажите путь к странице профиля
             ),
             IconButton(
                 icon=icons.ADMIN_PANEL_SETTINGS_OUTLINED,
                 tooltip="Админ Панель",
                 on_click=lambda e: page.go("/admin"),
-                visible=False if user_group != 'admin' else True
-            ),
-            Switch(
-                value=page.theme_mode == "dark",
-                on_change=toggle_theme,
-                label="Темная тема"
-            ),
-            IconButton(
-                icon=icons.COLOR_LENS,
-                tooltip="Сменить цветовую палитру",
-                on_click=change_color_palette
+                visible=False if user_group != 'admin' else True # Укажите путь к админ панели
             ),
         ],
     )
 
     def menu_clicked(e):
-        # Логика навигации
-        pass
+        if e.control.selected_index == 5:  # Кнопка "Logout"
+            if user_group != "guest":
+                page.session.clear()
+                page.client_storage.clear()
+                page.go("/login")
+                page.go('/index')# Переходим на страницу авторизации
+                page.update()
+            else:
+                page.go("/login")  # Переходим на страницу авторизации
+        if e.control.selected_index == 0:  # Кнопка "Главная"
+            page.go("/index")  # Переходим на страницу главной
+        if e.control.selected_index == 1:  # Кнопка "Серверы"
+            page.go("/servers")  # Переходим на страницу серверов
+        if e.control.selected_index == 2:  # Кнопка "Список банов"
+            page.go("/bans")  # Переходим на страницу списка банов
+        if e.control.selected_index == 3:  # Кнопка "Список Мутов/Гагов"
+            page.go("/mutes")  # Переходим на страницу списка Мутов/Гагов
+        if e.control.selected_index == 4:  # Кнопка "Админ-лист"
+            page.go("/admin_list")  # Переходим на страницу админ-листа
+
 
     rail = NavigationRail(
         destinations=[
-            # Элементы навигации
+            NavigationRailDestination(
+                icon=icons.HOME,
+                selected_icon=icons.HOME,
+                label="Главная"
+            ),
+            NavigationRailDestination(
+                icon=icons.CABLE_OUTLINED,
+                selected_icon=icons.CABLE_OUTLINED,
+                label="Серверы"
+            ),
+            NavigationRailDestination(
+                icon=icons.LOCK,
+                selected_icon=icons.LOCK,
+                label="Список банов"
+            ),
+            NavigationRailDestination(
+                icon=icons.VOICE_OVER_OFF,
+                selected_icon=icons.VOICE_OVER_OFF,
+                label="Список Мутов/Гагов"
+            ),
+            NavigationRailDestination(
+                icon=icons.STAR_HALF,
+                selected_icon=icons.STAR_HALF,
+                label="Админ-лист",
+            ),
+
+            NavigationRailDestination(
+                icon=icons.LOGOUT if user_group != "guest" else icons.LOGIN,  # Меняем иконку в зависимости от группы
+                selected_icon=icons.LOGOUT if user_group != "guest" else icons.LOGIN,
+                label="Logout" if user_group != "guest" else "Войти",  # Меняем текст в зависимости от группы
+            ),
         ],
         on_change=lambda e: menu_clicked(e),
     )
