b4fe67ed8738cceea687904316e2b74723638ca3
envi <alexgolubev1404@gmail.com>
2024-10-01 15:17:44 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -4,36 +4,28 @@ def tpl_index(page):
     page.title = 'Главная страница'
     page.theme_mode = ThemeMode.SYSTEM
 
-    def check_item_clicked(e):
-        e.control.checked = not e.control.checked
-        page.update()
+    page.horizontal_alignment = page.vertical_alignment = "center"
 
-    drawer = NavigationDrawer(
-        controls=[
-            Container(height=12),
-            TextButton(text="Item 1"),
-            Divider(thickness=2),
-        ],
-    )
+    page.floating_action_button = FloatingActionButton(icon=icons.ADD)
+    page.floating_action_button_location = FloatingActionButtonLocation.CENTER_DOCKED
+
     page.appbar = AppBar(
-        leading=IconButton(icons.MENU, on_click=lambda e: page.open(drawer)),
-        leading_width=40,
-        title=Text('SourceSMS', color=colors.ON_PRIMARY),
-        center_title=False,
-        bgcolor=colors.PRIMARY,
-        actions=[
-            IconButton(icons.WB_SUNNY_OUTLINED),
-            IconButton(icons.FILTER_3),
-            PopupMenuButton(
-                items=[
-                    PopupMenuItem(text="Item 1"),
-                    PopupMenuItem(),  # divider
-                    PopupMenuItem(
-                        text="Checked item", checked=False, on_click=check_item_clicked
-                    ),
-                ]
-            ),
-        ],
+        title=Text("Bottom AppBar Demo"),
+        center_title=True,
+        bgcolor=colors.GREEN_300,
+        automatically_imply_leading=False,
+    )
+    page.bottom_appbar = BottomAppBar(
+        bgcolor=colors.BLUE,
+        shape=NotchShape.CIRCULAR,
+        content=Row(
+            controls=[
+                IconButton(icon=icons.MENU, icon_color=colors.WHITE),
+                Container(expand=True),
+                IconButton(icon=icons.SEARCH, icon_color=colors.WHITE),
+                IconButton(icon=icons.FAVORITE, icon_color=colors.WHITE),
+            ]
+        ),
     )
 
     page.add(Text("Body!"))
