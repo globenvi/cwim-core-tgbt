4ff598c8df89cad7f93f47c962acf8834747902a
envi <alexgolubev1404@gmail.com>
2024-10-01 15:16:12 +0700

    Update index.py

--- templates/Default/index.py
+++ templates/Default/index.py
@@ -8,34 +8,11 @@ def tpl_index(page):
         e.control.checked = not e.control.checked
         page.update()
 
-    def handle_dismissal(e):
-        page.add(Text("Drawer dismissed"))
-
-    def handle_change(e):
-        page.add(Text(f"Selected Index changed: {e.selected_index}"))
-        # page.close(drawer)
-
     drawer = NavigationDrawer(
-        on_dismiss=handle_dismissal,
-        on_change=handle_change,
         controls=[
             Container(height=12),
-            NavigationDrawerDestination(
-                label="Item 1",
-                icon=icons.DOOR_BACK_DOOR_OUTLINED,
-                selected_icon_content=Icon(icons.DOOR_BACK_DOOR),
-            ),
+            TextButton(text="Item 1"),
             Divider(thickness=2),
-            NavigationDrawerDestination(
-                icon_content=Icon(icons.MAIL_OUTLINED),
-                label="Item 2",
-                selected_icon=icons.MAIL,
-            ),
-            NavigationDrawerDestination(
-                icon_content=Icon(icons.PHONE_OUTLINED),
-                label="Item 3",
-                selected_icon=icons.PHONE,
-            ),
         ],
     )
     page.appbar = AppBar(
