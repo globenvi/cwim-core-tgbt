1d2cad9e06061575ab67af4f8cafc12bd4c2e9fd
envi <alexgolubev1404@gmail.com>
2024-10-20 00:44:28 +0700

    Update admins_commands.py

--- core/handlers/admins_commands.py
+++ core/handlers/admins_commands.py
@@ -3,7 +3,7 @@ from aiogram import Router
 from aiogram.filters import Command
 
 from services.update_service import UpdateService
-from core.middlewares.is_admin import IsAdmin
+from core.middlewares.is_admin import isAdmin
 from core.keyboards.inline_keyboards import get_open_web_ui_keyboard
 
 from init import ngrok_url
@@ -12,12 +12,12 @@ router = Router()
 update_service = UpdateService()
 
 
-@router.message(Command('test_ui'), IsAdmin())
+@router.message(Command('test_ui'), isAdmin())
 async def test_ui_command(message):
     await message.answer("Тестирование Flet UI", reply_markup=get_open_web_ui_keyboard(message.from_user.id))
     await message.answer(f'DBG ngrok url: {ngrok_url}')
 
-@router.message(Command('get_updates'), IsAdmin())
+@router.message(Command('get_updates'), isAdmin())
 async def get_updates_command(message):
     updates = update_service.get_updates()
     if updates:
