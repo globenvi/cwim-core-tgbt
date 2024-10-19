0e3169feb038fa1d442decf77def25c57d2d050c
envi <alexgolubev1404@gmail.com>
2024-10-20 00:14:24 +0700

    Update admins_commands.py

--- core/handlers/admins_commands.py
+++ core/handlers/admins_commands.py
@@ -1,14 +1,16 @@
+# admin_commands.py
 from aiogram import Router
-
-from aiogram.types import Message
 from aiogram.filters import Command
-from pydantic.v1 import NoneStr
-
-from core.middlewares.is_admin import isAdmin
+from services.update_service import UpdateService
+from core.middlewares.is_admin import IsAdmin
 
 router = Router()
+update_service = UpdateService()
 
-@router.message(Command("create"))
-async def ban_user(message: Message):
-    pass
-"""Server creating"""
+@router.message(Command('get_updates'), IsAdmin())
+async def get_updates_command(message):
+    updates = update_service.get_updates()
+    if updates:
+        await message.answer(f"Доступные обновления:\n{updates}")
+    else:
+        await message.answer("Обновлений нет.")
