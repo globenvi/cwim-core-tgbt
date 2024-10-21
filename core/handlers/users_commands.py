673ea60c2da6b2883d9433035421d67c7bc85dec
envi <alexgolubev1404@gmail.com>
2024-10-21 13:34:52 +0700

    Update users_commands.py

--- core/handlers/users_commands.py
+++ core/handlers/users_commands.py
@@ -9,14 +9,3 @@ from core.controllers.UserController import User
 
 router = Router()
 
-@router.message(Command('start'))
-async def start_bot(message: Message):
-    user = User(message.from_user)
-    user_data = await user.read_user()
-
-    if not user_data:
-        await message.reply(f'Добро пожаловать {message.from_user.username}\n'
-                            f'Пожалуйста, ознакомься с правилами проекта! /rules', parse_mode="HTML")
-        await user.create('user', user_data)
-    else:
-        await message.reply(f'С возвраением!, {message.from_user.username}!')
