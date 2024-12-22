6e17f94e8826f52c3959624a737883be5be179bd
envi <alexgolubev1404@gmail.com>
2024-12-22 08:02:08 -0800

    Update core

--- config_reader.py
+++ config_reader.py
@@ -4,37 +4,22 @@ from dataclasses import dataclass
 
 @dataclass
 class Bots:
-    bot_tokken: str
-    admin_id: str
-    database: str
-    bot_name: str
-    chat_id: str
-    chat_name: str
-    group_id: str
-    group_name: str
+    token: str
 
 @dataclass
 class Settings:
     bots: Bots
 
 
-def get_settigns(path: str):
+def Config(path: str):
     env = Env()
     env.read_env(path)
 
     return Settings(
         bots=Bots(
-            bot_tokken=env.str("TOKKEN"),
-            admin_id=env.str("ADMIN_ID"),
-            database=env.str("DATABASE"),
-            bot_name=env.str("BOT_NAME"),
-            chat_id=env.str("CHAT_ID"),
-            chat_name=env.str("CHAT_NAME"),
-            group_id=env.str("GROUP_ID"),
-            group_name=env.str("GROUP_NAME"),
+            token=env.str("BOT_TOKEN"),
         )
     )
 
 
-settings = get_settigns(".env")
-print(settings)
+config = Config(".env")
--- core/keyboards/reply_keyboards.py
+++ core/keyboards/reply_keyboards.py
@@ -1,19 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
-
-main_keyboard = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="😎 Профиль")
-        ],
-        [
-            KeyboardButton(text="⚙ Настройки"),
-            KeyboardButton(text='💊 Тех.Поддержка')
-        ],
-    ],
-    resize_keyboard=True,
-    one_time_keyboard=True,
-    row_width=2,
-    selective=False,  # optional, defaults to False
-)
+# Главная клавиатура
+# start_keyboard = ReplyKeyboardMarkup(
+#     keyboard=[
+#         [
+#             KeyboardButton(text="📱 Ремонт iPhone"),
+#             KeyboardButton(text="🪶 Ремонт iPad")
+#         ],
+#         [
+#             KeyboardButton(text="⌚️ Ремонт iWatch"),
+#             KeyboardButton(text="🫧 Переклейка")
+#         ],
+#     ],
+#     resize_keyboard=True,
+#     one_time_keyboard=True,
+#     row_width=2,
+# )
